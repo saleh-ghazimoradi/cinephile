831a61c136820f237608fe4008d5c7c259257166
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-03 11:56:17 +0330

    improve the healthcheck of the project

--- internal/gateway/healthcheck.go
+++ internal/gateway/healthcheck.go
@@ -1,15 +1,21 @@
 package gateway
 
 import (
-	"fmt"
 	"github.com/saleh-ghazimoradi/cinephile/config"
+	"github.com/saleh-ghazimoradi/cinephile/logger"
 	"net/http"
 )
 
 const version = "1.0.0"
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "enviroment:%s\n", config.Appconfig.Env)
-	fmt.Fprintln(w, "version: "+version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": config.Appconfig.Env,
+		"version":     version,
+	}
+	if err := writeJSON(w, http.StatusOK, data, nil); err != nil {
+		logger.Logger.Error(err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
 }
