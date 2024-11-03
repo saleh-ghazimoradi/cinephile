d015f77a95f18ce0c897081e5d9562b562928185
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-03 13:31:41 +0330

    improve my models which takes both string and integer of the type runtime

--- internal/service/service_models/movies.go
+++ internal/service/service_models/movies.go
@@ -1,13 +1,27 @@
 package service_models
 
-import "time"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Runtime int32
+
+func (r Runtime) MarshalJSON() ([]byte, error) {
+	jsonValue := fmt.Sprintf("%d mins", r)
+
+	quotedJSONValue := strconv.Quote(jsonValue)
+
+	return []byte(quotedJSONValue), nil
+}
 
 type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	Runtime   int32     `json:"runtime,omitempty"`
+	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }
