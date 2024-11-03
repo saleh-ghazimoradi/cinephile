2139dbd48630f893e38ddf3bb7ca9cd87c624936
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-03 11:57:01 +0330

    add movie routes to the Routes function

--- internal/gateway/routes.go
+++ internal/gateway/routes.go
@@ -7,12 +7,16 @@ import (
 
 type Handlers struct {
 	HealthCheckHandler http.HandlerFunc
+	ShowMovieHandler   http.HandlerFunc
+	CreateMovieHandler http.HandlerFunc
 }
 
 func Routes(handlers Handlers) http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", handlers.HealthCheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", handlers.ShowMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", handlers.CreateMovieHandler)
 
 	return router
 }
