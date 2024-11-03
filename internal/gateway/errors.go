e5a51d4cbe8b2343429ca96bc62ae19393729ca0
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-03 16:03:07 +0330

    create some functions for errors to be written in JSON format

---
+++ internal/gateway/errors.go
@@ -0,0 +1,41 @@
+package gateway
+
+import (
+	"fmt"
+	"github.com/saleh-ghazimoradi/cinephile/logger"
+	"net/http"
+)
+
+func logError(r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+	logger.Logger.Error(err.Error(), "method", method, "uri", uri)
+}
+
+func errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := envelope{"error": message}
+
+	err := writeJSON(w, status, env, nil)
+	if err != nil {
+		logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logError(r, err)
+	message := "the server encountered a problem and could not process your request"
+	errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not allowed", r.Method)
+	errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
