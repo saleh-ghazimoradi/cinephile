85137a2165fb5b6bcb151c7dcda39e630b692887
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-03 10:42:59 +0330

    implement cmd http functionality

--- cmd/http.go
+++ cmd/http.go
@@ -1,40 +1,31 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/saleh-ghazimoradi/cinephile/config"
+	"github.com/saleh-ghazimoradi/cinephile/internal/gateway"
+	"github.com/saleh-ghazimoradi/cinephile/logger"
 	"github.com/spf13/cobra"
 )
 
 // httpCmd represents the http command
 var httpCmd = &cobra.Command{
 	Use:   "http",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "launching the http rest listen server",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("http called")
+		logger.Logger.Info("server has started", "addr", config.Appconfig.ServerAddress, "env", config.Appconfig.Env)
+
+		routesHandler := gateway.Handlers{
+			HealthCheckHandler: gateway.HealthCheckHandler,
+		}
+
+		if err := gateway.Server(gateway.Routes(routesHandler)); err != nil {
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(httpCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// httpCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// httpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
