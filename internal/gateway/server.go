f1a3a12e3251b04439fa9af40606067f7a1cc042
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-03 10:43:38 +0330

    implement server

---
+++ internal/gateway/server.go
@@ -0,0 +1,29 @@
+package gateway
+
+import (
+	"fmt"
+	"github.com/saleh-ghazimoradi/cinephile/config"
+	"github.com/saleh-ghazimoradi/cinephile/logger"
+	"net/http"
+	"os"
+	"time"
+)
+
+func Server(mux http.Handler) error {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%s", config.Appconfig.ServerAddress),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	logger.Logger.Info("starting server", "addr", srv.Addr, "env", config.Appconfig.Env)
+
+	if err := srv.ListenAndServe(); err != nil {
+		logger.Logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	return nil
+}
