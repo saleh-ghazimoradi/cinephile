756d4070e38f9dace199008cdc80d9e2c282a9df
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-05 12:12:04 +0330

    remove the runtime functionality for showing the mins

--- internal/service/service_models/movies.go
+++ internal/service/service_models/movies.go
@@ -1,27 +1,15 @@
 package service_models
 
 import (
-	"fmt"
-	"strconv"
 	"time"
 )
-
-type Runtime int32
-
-func (r Runtime) MarshalJSON() ([]byte, error) {
-	jsonValue := fmt.Sprintf("%d mins", r)
-
-	quotedJSONValue := strconv.Quote(jsonValue)
-
-	return []byte(quotedJSONValue), nil
-}
 
 type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`
-	Runtime   Runtime   `json:"runtime,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }
