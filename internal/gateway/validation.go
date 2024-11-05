40cd28d20a1b1f76f5ee201b97716d8928206c33
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-05 12:11:29 +0330

    improve the validation.go file

--- internal/gateway/validation.go
+++ internal/gateway/validation.go
@@ -2,4 +2,8 @@ package gateway
 
 import "github.com/go-playground/validator/v10"
 
-var Validate = validator.New()
+var Validate *validator.Validate
+
+func init() {
+	Validate = validator.New(validator.WithRequiredStructEnabled())
+}
