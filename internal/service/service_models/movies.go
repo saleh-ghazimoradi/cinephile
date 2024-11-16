d5dc0be6a09e9660819ca2397cc6628e5ec22627
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-16 10:12:01 +0330

    implement the pagination stuff and movie payload

--- internal/service/service_models/movies.go
+++ internal/service/service_models/movies.go
@@ -13,3 +13,34 @@ type Movie struct {
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
 }
+
+type MoviePayload struct {
+	Title   string   `json:"title"`
+	Year    int32    `json:"year"`
+	Runtime int32    `json:"runtime"`
+	Genres  []string `json:"genres"`
+}
+
+type UpdateMoviePayload struct {
+	Title   *string  `json:"title"`
+	Year    *int32   `json:"year"`
+	Runtime *int32   `json:"runtime"`
+	Genres  []string `json:"genres"`
+}
+
+type WithPagination struct {
+	Title  string   `json:"title"`
+	Genres []string `json:"genres"`
+	Filter `json:"filter"`
+}
+
+type Filter struct {
+	Page     int    `json:"page" validate:"gte=0,lte=10000000"`
+	PageSize int    `json:"page_size" validate:"gte=1,lte=20"`
+	Sort     string `json:"sort" validate:"oneof=asc desc"`
+}
+
+type MovieWithMetaData struct {
+	Movie
+	MovieCount int `json:"movie_count"`
+}
