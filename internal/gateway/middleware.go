bd6a85b2783d36fa6c9841c96b5eb91348d2679a
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-16 18:41:16 +0330

    use the token bucket rate limiter algorithms to implement rate limit functionality

--- internal/gateway/middleware.go
+++ internal/gateway/middleware.go
@@ -2,7 +2,12 @@ package gateway
 
 import (
 	"fmt"
+	"github.com/saleh-ghazimoradi/cinephile/config"
+	"golang.org/x/time/rate"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 )
 
 func recoverPanic(next http.Handler) http.Handler {
@@ -13,6 +18,64 @@ func recoverPanic(next http.Handler) http.Handler {
 				serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func rateLimit(next http.Handler) http.Handler {
+
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if config.Appconfig.RateLimitEnabled {
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				serverErrorResponse(w, r, err)
+				return
+			}
+
+			mu.Lock()
+
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{
+					limiter: rate.NewLimiter(rate.Limit(config.Appconfig.RPS), config.Appconfig.Burst),
+				}
+			}
+
+			clients[ip].lastSeen = time.Now()
+
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				rateLimitExceededResponse(w, r)
+				return
+			}
+
+			mu.Unlock()
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
