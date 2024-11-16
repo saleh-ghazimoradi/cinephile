8ccd88b59644aae1714ef2e85774f53a6b919da9
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-16 19:21:14 +0330

    fully implement the graceful shutdown

--- internal/gateway/server.go
+++ internal/gateway/server.go
@@ -1,11 +1,15 @@
 package gateway
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"github.com/saleh-ghazimoradi/cinephile/config"
 	"github.com/saleh-ghazimoradi/cinephile/logger"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -18,12 +22,34 @@ func Server(router http.Handler) error {
 		WriteTimeout: 10 * time.Second,
 	}
 
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		logger.Logger.Info("shutting down server", "signal", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
 	logger.Logger.Info("starting server", "addr", srv.Addr, "env", config.Appconfig.Env)
 
-	if err := srv.ListenAndServe(); err != nil {
-		logger.Logger.Error(err.Error())
-		os.Exit(1)
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
 	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	logger.Logger.Info("stopped server", "addr", srv.Addr)
 
 	return nil
 }
