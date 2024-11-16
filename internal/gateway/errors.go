76ac484312c21d79b361681eb7eec5b4fd0cac2a
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-16 18:39:52 +0330

    create a ratelimit error function in the errors.go in the transfer layer

--- internal/gateway/errors.go
+++ internal/gateway/errors.go
@@ -52,3 +52,8 @@ func editConflictResponse(w http.ResponseWriter, r *http.Request) {
 	message := "unable to update the record due to an edit conflict, please try again"
 	errorResponse(w, r, http.StatusConflict, message)
 }
+
+func rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
+	message := "rate limit exceeded"
+	errorResponse(w, r, http.StatusTooManyRequests, message)
+}
