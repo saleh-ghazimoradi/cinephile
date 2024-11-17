1720f31fd381979cbbf49196da425b704cd34e7b
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-17 12:59:11 +0330

    add user model

---
+++ internal/service/service_models/user.go
@@ -0,0 +1,51 @@
+package service_models
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  Password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+type UserPayload struct {
+	Name     string `json:"name" validate:"required,max=100"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=72"`
+}
+
+type Password struct {
+	Plaintext *string
+	Hash      []byte
+}
+
+func (p *Password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+	p.Plaintext = &plaintextPassword
+	p.Hash = hash
+	return nil
+}
+
+func (p *Password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.Hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
+}
